c3cd3aa14724fab8412327895a05d44c0cb59578
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-07-16 16:16:19 -0700

    Header component shell

---
+++ src/components/common/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header>
+      <h1>HYDRA</h1>
+      <p>A unique dice rolling experience</p>
+    </header>
+  );
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import Header from '../components/common/Header';
 
 export default function Home() {
   return (
     <>
-      <h1>HYDRA</h1>
-      <p>A unique dice rolling experience</p>
+      <Header />
     </>
   );
 }
