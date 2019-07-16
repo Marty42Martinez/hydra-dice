086e3272aeac3e4ea6f0580be37feaf1d96538de
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-07-16 16:45:29 -0700

    Header with simple nav links

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
     <header>
-      <h1>HYDRA</h1>
+      <Link to="/"><h1>HYDRA</h1></Link>
       <p>A unique dice rolling experience</p>
+      <Link to="/rollspace"><p>Roll Dice</p></Link>
     </header>
   );
 }
