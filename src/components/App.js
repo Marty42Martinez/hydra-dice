0b075ecd1319b280f76eba291372c61ad4d99477
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-07-16 16:46:19 -0700

    Links and Routes connect components through App

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
 import Home from '../pages/Home';
+import RollSpace from '../pages/RollSpace';
 
 export default function App() {
   return (
-    <>
-      <Home />
-    </>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rollspace" component={RollSpace} />
+      </Switch>
+    </Router>
   );
 }
-  
--- src/pages/RollSpace.js
+++ src/pages/RollSpace.js
@@ -2,11 +2,11 @@ import React from 'react';
 import Header from '../components/common/Header';
 import Footer from '../components/common/Footer';
 
-export default function Home() {
+export default function RollSpace() {
   return (
     <>
       <Header />
-      <p>This is the Home Page</p>
+      <p>This is where we roll dice</p>
       <Footer />
     </>
   );
